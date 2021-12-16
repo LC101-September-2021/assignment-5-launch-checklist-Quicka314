675d9485fe6dc521ee540b9853d0ec2e1b134022
Quicka314 <93557196+Quicka314@users.noreply.github.com>
2021-12-16 15:33:14 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,25 +2,15 @@
 
 window.addEventListener("load", function() {
     let document = window.document;
-
     let pilotStatus = document.querySelector("li[id=pilotStatus]");
-
     let copilotStatus = document.querySelector("li[id=copilotStatus]");
-
     let list = document.querySelector("div[id=faultyItems]");
-
     list.style.visibility = "hidden";
-
     let form = document.querySelector("form");
-
     let pilotName = document.querySelector("input[name=pilotName]");
-
     let copilotName = document.querySelector("input[name=copilotName]");
-
     let fuelLevel = document.querySelector("input[name=fuelLevel]");
-
     let cargoMass = document.querySelector("input[name=cargoMass]");
-
     let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
@@ -33,21 +23,13 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
 
             let missionTarget = document.getElementById("missionTarget");
-
             let planet = pickPlanet(listedPlanets);
-
             let name = planet.name;
-
             let diameter = planet.diameter;
-
             let star = planet.star;
-
             let distance = planet.distance;
-
             let moons = planet.moons;
-
             let imageUrl = planet.image;
-
             addDestinationInfo(window.document, name, diameter, star, distance, moons, imageUrl)
    });
 
@@ -55,49 +37,34 @@ window.addEventListener("load", function() {
     form.addEventListener("submit", function
         (event){
             let document = window.document;
-
             list.style.visibility = "hidden";
-
             if (pilotName.value === '' || copilotName.value === '' || fuelLevel.value === '' || cargoMass.value === ''){
                 alert("All fields are required!");
-
                 event.preventDefault();
-
            return;
             } else if (validateInput(pilotName.value) === "Is a Number") {
-        
             alert("Make sure to enter valid information for each field!");
-            
             event.preventDefault();
             return;
         } else if (validateInput(copilotName.value) === "Is a Number") {
             alert("Make sure to enter valid information for each field!");
-            
             event.preventDefault();
             return;
         } else if (validateInput(fuelLevel.value) !== "Is a Number") {
             alert("Make sure to enter valid information for each field!");
-            
             event.preventDefault();
             return;
         } else if (validateInput(cargoMass.value) === "Not a Number") {
             alert("Make sure to enter valid information for each field!");
-            
             event.preventDefault();
             return;
         } else {
             let pilotStatus = window.document.querySelector("li[id=pilotStatus]");
-
             let copilotStatus = window.document.querySelector("li[id=copilotStatus]");
-
             let pilotName = window.document.querySelector("input[name=pilotName]");
-
             let copilotName = window.document.querySelector("input[name=copilotName]");
-
             let cargoMass = window.document.querySelector("input[name=cargoMass]");
-
             formSubmission(window.document, list, pilotName.value, copilotName.value, fuelLevel.value, cargoMass.value);
-
             event.preventDefault();
         }
 
